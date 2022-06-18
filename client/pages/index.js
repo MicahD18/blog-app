16b934d38b7c2e095ecb057f711dd29297ad0b9b
MicahD18 <micah.daise@nwktc.edu>
2022-06-18 16:23:59 -0600

    added code that fetches data from GraphCMS project and created post card component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
+import { getPosts } from "../services";
 
 import { PostCard, Categories, PostWidget } from "../components";
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-];
-
-export default function Home() {
+// we get posts from getStaticProps function
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -18,7 +15,7 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -31,3 +28,13 @@ export default function Home() {
     </div>
   );
 };
+
+// fetch data in Next.js
+// (same as useEffect in React.js)
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+
+  return {
+    props: { posts }
+  }
+}
